4e990251e14faf6403ca7c311fd4332e9943af30
robinsonclo1 <robinson.clower@gmail.com>
2018-04-15 15:22:05 -0500

    secured alterEvent and newEvent

--- postLogin/alterEvent.php
+++ postLogin/alterEvent.php
@@ -10,19 +10,20 @@ if(isset($_POST['eventName'])) {
   $location  = trim(htmlspecialchars($_POST['eventLocation'])) ?? '';
   $id = $_POST['id'];
 
-  $updateEventSQL = "UPDATE EventInfo set
-    eventDate = '$eventDate',
-    eventName = '$eventName',
-    eventLocation = '$location',
-    numRounds = $numRounds
-    WHERE eventID_PK = $id;";
+  $updateEventSQL = $conn->prepare("UPDATE EventInfo set
+    eventDate = ?,
+    eventName = ?,
+    eventLocation = ?,
+    numRounds = ?
+    WHERE eventID_PK = $id;");
+  $updateEventSQL->bind_param("sssi", $eventDate, $eventName, $location, $numRounds);
 
-	if ($conn->query($updateEventSQL) === TRUE) {
-		echo "<script type='text/javascript'>console.log('ignore')</script>";
+	if ($updateEventSQL->execute() === TRUE) {
+    $updateEventSQL->close();
 	} else {
 		 die('Invalid query: ' . mysql_error());
 	}
 } else {
-  echo "<script type='text/javascript'>alert('didn'twork);</script>";
+  echo "<script type='text/javascript'>alert('didn't work);</script>";
 }
 ?>
